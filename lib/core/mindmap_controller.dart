2e89e113fc61c0710b583b6177ce6e775b379817
Tommi Enenkel <tommi.enenkel@cssteam.at>
2014-05-06 18:47:02 +0200

    clean up repo

--- lib/core/mindmap_controller.dart
+++
@@ -1,6 +0,0 @@
-part of mindmap_core;
-
-abstract class MindmapController {
-  Node addNode(num x, num y);
-  addChildNode(Node parent);
-}
--- lib/core/mindmap_core.dart
+++
@@ -1,6 +0,0 @@
-library mindmap_core;
-
-part "graph.dart";
-part "node.dart";
-part "edge.dart";
-part "mindmap_controller.dart";
